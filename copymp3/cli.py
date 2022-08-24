e857417d3b0e04047af84411ca636f03c0ebcfed
Rafael Delgado Martins <rafael@delgadomartins.com>
2022-08-24 14:08:52 -0300

    WIP

---
+++ copymp3/cli.py
@@ -0,0 +1,31 @@
+'''
+copymp3: Copy MP3 files to another directory
+Copyright (C) 2022  Rafael Delgado Martins
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+
+def main():
+    ''' entrypoint '''
+    print('''
+        copymp3 Copyright (C) 2022  Rafael Delgado Martins
+        This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+        This is free software, and you are welcome to redistribute it
+        under certain conditions; type `show c' for details.
+    ''')
+
+
+if __name__ == '__main__':
+    main()
